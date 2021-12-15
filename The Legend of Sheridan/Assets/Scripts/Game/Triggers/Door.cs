1cb36e5b837182c9df250608711347ece071c95c
Yash Mandloi <yashmandloi@gmail.com>
2021-12-15 07:06:27 -0500

    Free door functionality and Sound manager system

--- The Legend of Sheridan/Assets/Scripts/Game/Triggers/Door.cs
+++ The Legend of Sheridan/Assets/Scripts/Game/Triggers/Door.cs
@@ -7,10 +7,14 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    public bool DoorOpenInAxisX;
+
     // Initialize
     [SerializeField] Transform DoorLeft;
     [SerializeField] Transform DoorRight;
 
+    float doorLeftPosX;
+    float doorRightPosX;
     float doorLeftPosZ;
     float doorRightPosZ;
 
@@ -19,6 +23,8 @@ public class Door : MonoBehaviour
     void Start()
     {
         // Initialize the left and right doors' position Z
+        doorLeftPosX = DoorLeft.position.x;
+        doorRightPosX = DoorRight.position.x;
         doorLeftPosZ = DoorLeft.position.z;
         doorRightPosZ = DoorRight.position.z;
     }
@@ -29,8 +35,16 @@ public class Door : MonoBehaviour
         if (openDoor == true)
         {
             // "Open" the door for player with change in positions
-            DoorLeft.position = Vector3.Lerp(DoorLeft.transform.position, new Vector3(DoorLeft.transform.position.x, DoorLeft.transform.position.y, doorLeftPosZ - 1.5f), 10f);
-            DoorRight.position = Vector3.Lerp(DoorRight.transform.position, new Vector3(DoorRight.transform.position.x, DoorRight.transform.position.y, doorRightPosZ + 1.5f), 10f);
+            if (DoorOpenInAxisX)
+            {
+                DoorLeft.position = Vector3.Lerp(DoorLeft.transform.position, new Vector3(doorLeftPosX + 1.5f, DoorLeft.transform.position.y, DoorLeft.transform.position.z), 10f);
+                DoorRight.position = Vector3.Lerp(DoorRight.transform.position, new Vector3(doorRightPosX - 1.5f, DoorRight.transform.position.y, DoorLeft.transform.position.z), 10f);
+            }
+            else
+            {
+                DoorLeft.position = Vector3.Lerp(DoorLeft.transform.position, new Vector3(DoorLeft.transform.position.x, DoorLeft.transform.position.y, doorLeftPosZ - 1.5f), 10f);
+                DoorRight.position = Vector3.Lerp(DoorRight.transform.position, new Vector3(DoorRight.transform.position.x, DoorRight.transform.position.y, doorRightPosZ + 1.5f), 10f);
+            }
         }
     }
     
@@ -40,7 +54,7 @@ public class Door : MonoBehaviour
         if (other.gameObject.name == "Zelda")
         {
             // If Zelda has the key in his inventory
-            if (other.gameObject.GetComponent<Inventory>().hasKey == true)
+            if (other.gameObject.GetComponent<Inventory>().hasKey == true || this.tag == "FreeDoor")
             {
                 // Set openDoor to true
                 openDoor = true;
